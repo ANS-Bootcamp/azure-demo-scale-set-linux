f450215c70ff228e290b86a0c7f73e6d36e07de0
nathanguk <nathan.gaskill@ansgroup.co.uk>
2018-02-27 16:55:57 +0000

    exports

--- scripts/azure-demo-scale-set.sh
+++ scripts/azure-demo-scale-set.sh
@@ -5,6 +5,10 @@ echo "PORT=80" >> /etc/environment
 echo "AZURE_STORAGE_ACCOUNT=$1" >> /etc/environment
 echo "AZURE_STORAGE_ACCESS_KEY=$2" >> /etc/environment
 
+export "PORT=80"
+export "AZURE_STORAGE_ACCOUNT=$1"
+export "AZURE_STORAGE_ACCESS_KEY=$2"
+
 # Configure Script Variables
 path="/ans"
 gitAccount="nathanguk"
