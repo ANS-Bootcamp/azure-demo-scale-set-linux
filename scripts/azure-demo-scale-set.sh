0259bb3297ecea746c878b69cb57d4e17f15567c
nathanguk <nathan.gaskill@ansgroup.co.uk>
2018-02-28 17:09:35 +0000

    updates

--- scripts/azure-demo-scale-set.sh
+++ scripts/azure-demo-scale-set.sh
@@ -3,11 +3,13 @@
 # Configure Environment Variables
 echo "PORT=80" >> /etc/environment
 echo "STATUS=200" >> /etc/environment
+echo "CPU=false" >> /etc/environment
 echo "AZURE_STORAGE_ACCOUNT=$1" >> /etc/environment
 echo "AZURE_STORAGE_ACCESS_KEY=$2" >> /etc/environment
 
 export "PORT=80"
 export "STATUS=200"
+export "CPU=false"
 export "AZURE_STORAGE_ACCOUNT=$1"
 export "AZURE_STORAGE_ACCESS_KEY=$2"
 
@@ -28,6 +30,9 @@ apt-get install -y nodejs
 # Install Unzip
 apt-get install -y unzip
 
+# Install Stress-Ng
+apt-get install -y stress-ng
+
 # Download and unzip application files
 mkdir $path
 resourcePath="/$gitAccount/$gitRepo/archive/master.zip"
@@ -36,8 +41,14 @@ url="https://github.com$resourcePath"
 curl -sL $url -o "/$path/master.zip"
 unzip "$path/master.zip" -d $path
 
+# Configure application as node service
 npm install node-linux --prefix "$path/$gitRepo-master"
 npm install --prefix "$path/$gitRepo-master"
 node "$path/$gitRepo-master/service.js"
-
 service $service start
+
+# Brings loadbalancer back into service every 5 minutes
+(crontab -l 2>/dev/null; echo "*/5 * * * * curl -s http://127.0.0.1/200 && curl -s http://127.0.0.1/cpulow") | crontab -
+
+# Checks is CPU stress process needs loading every 1 minutes
+(crontab -l 2>/dev/null; echo "*/1 * * * * if curl -s http://127.0.0.1/cpu | head -n 3 | grep -q 'true' ; then stress-ng -c 0 -l 75 -t 60s ; fi") | crontab -
