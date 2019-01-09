881fefb918bf69d34229224b04fcc739e823fa6c
Nathan Gaskill <15169394+nathanguk@users.noreply.github.com>
2019-01-09 19:23:16 +0000

    Update azure-demo-scale-set.sh
    Step 3 - Troubleshooting

--- scripts/azure-demo-scale-set.sh
+++ scripts/azure-demo-scale-set.sh
@@ -17,8 +17,38 @@ service="azuredemogallery"
 
 #Update Server
 apt-get update -y
-apt-get upgrade -y
+#apt-get upgrade -y
 
+# Download and install Node.JS
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get install -y nodejs
+
+# Install Unzip
+apt-get install -y unzip
+
+# Install Stress-Ng
+apt-get install -y stress-ng
+
+# Download and unzip application files
+mkdir $path
+resourcePath="/$gitAccount/$gitRepo/archive/master.zip"
+url="https://github.com$resourcePath" 
+
+curl -sL $url -o "/$path/master.zip"
+unzip "$path/master.zip" -d $path
+
+# Configure application as node service
+npm install node-linux --prefix "$path/$gitRepo-master"
+npm install --prefix "$path/$gitRepo-master"
+node "$path/$gitRepo-master/service.js"
+
+service $service start
+
+# Workaround for first boot issue.
+sleep 10s
+service $service stop
+sleep 10s
+service $service start
 
 
 
